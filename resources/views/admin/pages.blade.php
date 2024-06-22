121a6ea5772a09d65c32700af3fda25f0407d7f9
Samir Nabil <sameer.arcons@yahoo.co.uk>
2024-06-22 10:21:20 +0300

    add upload images to tinymce

--- resources/views/admin/pages.blade.php
+++ resources/views/admin/pages.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.admin')
- 
+
 @section('title', $data->page)
 
 @section('content')
@@ -14,8 +14,8 @@
                     <div class="input-group">
                         <input type="text" class="form-control" placeholder="Search for...">
                         <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
+                            <button class="btn btn-default" type="button">Go!</button>
+                        </span>
                     </div>
                 </div>
             </div>
@@ -23,31 +23,32 @@
 
         <div class="clearfix"></div>
         @if (session('message'))
-            <div id="back-message" class="alert alert-success alert-dismissible fade in" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <strong>Oh yeah!</strong> {{session('message')}}
-             </div>
+        <div id="back-message" class="alert alert-success alert-dismissible fade in" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
+            <strong>Oh yeah!</strong> {{session('message')}}
+        </div>
         @endif
         @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
+        <div class="alert alert-danger">
+            <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                <li>{{ $error }}</li>
                 @endforeach
-                </ul>
-            </div>
+            </ul>
+        </div>
         @endif
         <div class="clearfix"></div>
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>{{ Str::title($data->page) }} <small>after make your changes press save change buttom to take active</small></h2>
+                        <h2>{{ Str::title($data->page) }} <small>after make your changes press save change buttom to take
+                                active</small></h2>
                         <ul class="nav navbar-right panel_toolbox">
                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                             </li>
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>                                
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
                             </li>
                             <li><a class="close-link"><i class="fa fa-close"></i></a>
                             </li>
@@ -57,8 +58,8 @@
                     <div class="x_content">
                         <br>
                         <form id="site-pages" action="{{ route('pages.update', $data) }}" class="form-horizontal form-label-left" novalidate="" method="POST">
-                            {{ csrf_field() }}                            
-                            
+                            {{ csrf_field() }}
+
                             <input type="hidden" name="_method" value="PATCH">
                             <div class="form-group">
                                 <label class="control-label col-md-2 col-sm-2 col-xs-12" for="{{$data->page}}">{{ Str::title($data->page) }}</label>
@@ -68,7 +69,7 @@
                                     @else
                                     <textarea type="text" name="content" id="{{$data->page}}" class="form-control col-md-7 col-xs-12" row="10">{{$data->content}}</textarea>
                                     <textarea type="text" name="content_ar" id="{{$data->page}}" class="form-control col-md-7 col-xs-12" row="10">{{$data->content_ar}}</textarea>
-                                    @endif                                    
+                                    @endif
                                 </div>
                             </div>
                             <input type="hidden" name="page" value="{{$data->page}}">
@@ -88,27 +89,52 @@
 @endsection
 
 @section('jsFiles')
-    
-    <!-- iCheck -->
-    <script src="{{asset('vendors/iCheck/icheck.min.js')}}"></script>
-	<script src="{{asset('vendors/tinymce/js/tinymce/tinymce.min.js')}}"></script>
-	<script>
-        tinymce.init({
+
+<!-- iCheck -->
+<script src="{{asset('vendors/iCheck/icheck.min.js')}}"></script>
+<script src="{{asset('vendors/tinymce/js/tinymce/tinymce.min.js')}}"></script>
+<script>
+    tinymce.init({
         selector: 'textarea',
         height: 200,
+        images_upload_url: '/',
         theme: 'modern',
         plugins: 'code print preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help paste',
         toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link media image | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat | code',
         image_advtab: true,
-        paste_enable_default_filters: false,
-        //paste_as_text: true,
-            relative_urls : false,
-            remove_script_host : false,
-            convert_urls : true, 
+        relative_urls: false,
+        remove_script_host: false,
+        document_base_url: "{{url('/uploads/')}}",
+
         content_css: [
-        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i'
-        ]
-      });
-	</script>
+            '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i'
+        ],
+        images_upload_handler: function(blobInfo, success, failure) {
+            var xhr, formData;
+            xhr = new XMLHttpRequest();
+            xhr.withCredentials = false;
+            xhr.open('POST', "{{ url('/admin/upload/img') }}");
+            var token = '{{ csrf_token() }}';
+            xhr.setRequestHeader("X-CSRF-Token", token);
+            xhr.onload = function() {
+                var json;
+                if (xhr.status != 200) {
+                    failure('HTTP Error: ' + xhr.status);
+                    return;
+                }
+                json = JSON.parse(xhr.responseText);
+
+                if (!json || typeof json.location != 'string') {
+                    failure('Invalid JSON: ' + xhr.responseText);
+                    return;
+                }
+                success(json.location);
+            };
+            formData = new FormData();
+            formData.append('file', blobInfo.blob(), blobInfo.filename());
+            xhr.send(formData);
+        }
+    });
+</script>
 
 @endsection
